77d33df32946d683be3880c1dc83dac63cdf8774
Mohammed Shamaa <mohammed-shamaa@users.noreply.github.com>
2026-08-25 16:35:29 +0300

    fix: populate AISessionStorage migration and add auto-migrate on startup

--- src/Wesal.API/Program.cs
+++ src/Wesal.API/Program.cs
@@ -101,6 +101,12 @@ try
 
     var app = builder.Build();
 
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        db.Database.Migrate();
+    }
+
     app.UseSerilogRequestLogging();
 
     app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
